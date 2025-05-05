276be2f3016487d6e23d1bacb6274f3355a2558c
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-05-05 23:37:52 +0400

    feat: add join message

--- src/events/GuildMemberAdd.ts
+++ src/events/GuildMemberAdd.ts
@@ -1,11 +1,16 @@
-import { Events, GuildMember } from "discord.js";
+import { Events, GuildFeature, GuildMember } from "discord.js";
 import { Event } from "../types/Event";
 
 const GuildMemberAdd: Event = {
   name: Events.GuildMemberAdd,
   once: false,
   execute(member: GuildMember) {
-    console.log(`New member joined: ${member.user.tag}`);
+    if (
+      member.guild.features.includes(GuildFeature.MemberVerificationGateEnabled) ||
+      !member.guild.systemChannel
+    ) return;
+
+    member.guild.systemChannel.send(`Welcome to ${member.guild.name}, <@${member.id}>! Please read the rules and guidelines before chatting.`);
   }
 }
 
