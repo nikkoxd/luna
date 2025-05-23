5da95977e3095fd8322893e1bd10116c54277afe
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-05-24 02:57:20 +0400

    fix: add data validation and confirmation reply

--- src/commands/import.ts
+++ src/commands/import.ts
@@ -1,8 +1,8 @@
-import { ChatInputCommandInteraction, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ChatInputCommandInteraction, MessageFlags, PermissionFlagsBits, SlashCommandBuilder, TextDisplayBuilder } from "discord.js";
 import { Command } from "../types/Command";
-import { MongoDBMember } from "../types/MongoDBMember";
 import { bot } from "..";
 import { members } from "../schema";
+import { z } from "zod";
 
 const ImportCommand: Command = {
   data: new SlashCommandBuilder()
@@ -29,28 +29,88 @@ const ImportCommand: Command = {
         interaction.reply("Invalid file type. Please upload a JSON file.");
         return;
       }
-      interaction.reply("Importing data...");
 
       const response = await fetch(file.url);
-      const unparsedData = await response.json();
-      const data: MongoDBMember[] = JSON.parse(unparsedData);
+      const membersJSON = await response.json();
 
-      for (const member of data) {
-        await bot.drizzle
-          .insert(members)
-          .values({
-            id: Number(member.memberId),
-            guildId: Number(interaction.guildId),
-            exp: member.exp,
-            balance: member.coins,
-          })
-          .onConflictDoUpdate({
-            target: [members.id, members.guildId],
-            set: { exp: member.exp, balance: member.coins },
-          })
+      const mongoSchema = z.array(
+        z.object({
+          _id: z.object({
+            $oid: z.string(),
+          }),
+          memberId: z.string(),
+          coins: z.number(),
+          roles: z.array(z.object({
+            guildId: z.string(),
+            roleId: z.string(),
+            expiryDate: z.number(),
+          })),
+          __v: z.number(),
+          exp: z.number(),
+          level: z.number(),
+          rooms: z.array(z.string()).optional(),
+        })
+      );
+      const parsedMembers = mongoSchema.safeParse(membersJSON);
+
+      if (!parsedMembers.success) {
+        interaction.reply({ content: "Invalid JSON format. Use only with data exported from MongoDB.", flags: [MessageFlags.Ephemeral] });
+        return;
       }
+      await interaction.reply({ content: "Importing data...", flags: [MessageFlags.Ephemeral] });
 
-      interaction.editReply("Imported data from MongoDB.");
+      if (parsedMembers.data) {
+        const text = [
+          `About to import **${parsedMembers.data.length} members** to guild ${interaction.guild?.name}.`,
+          `Are you sure you want to continue?`,
+        ].join("\n");
+
+        const actionRow = new ActionRowBuilder<ButtonBuilder>()
+
+        const confirmButton = new ButtonBuilder()
+          .setLabel("Confirm")
+          .setStyle(ButtonStyle.Danger)
+          .setCustomId("confirm");
+        actionRow.addComponents(confirmButton);
+
+        const response = await interaction.editReply({
+          content: text,
+          components: [actionRow],
+        });
+
+        try {
+          const confirmation = await response.awaitMessageComponent({
+            filter: i => i.user.id === interaction.user.id,
+            time: 60_000,
+          })
+
+          if (confirmation.customId === "confirm") {
+            for (const member of parsedMembers.data) {
+              try {
+                await bot.drizzle
+                  .insert(members)
+                  .values({
+                    id: Number(member.memberId),
+                    guildId: Number(interaction.guildId),
+                    exp: member.exp,
+                    balance: member.coins,
+                  })
+                  .onConflictDoUpdate({
+                    target: [members.id, members.guildId],
+                    set: { exp: member.exp, balance: member.coins },
+                  })
+              } catch (error) {
+                confirmation.reply({ content: `Error importing member ${member.memberId}: ${error}`, flags: [MessageFlags.Ephemeral] });
+                return;
+              }
+            }
+          }
+
+          confirmation.reply({ content: "Imported data from MongoDB.", flags: [MessageFlags.Ephemeral] });
+        } catch {
+          interaction.editReply({ content: "Import cancelled.", components: [] });
+        }
+      }
     }
   }
 }
