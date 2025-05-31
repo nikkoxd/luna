afdc8e0301e1e49c21024d917b6d05bcc8569650
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-05-31 16:28:47 +0400

    fix: return en if guild locale not found

--- src/utils.ts
+++ src/utils.ts
@@ -2,11 +2,12 @@ import { eq } from "drizzle-orm";
 import { bot } from ".";
 import { guilds } from "./schema";
 
-export async function getGuildLocale(guildId: string): Promise<string | undefined> {
-  const config = await bot.drizzle
+export async function getGuildLocale(guildId: string): Promise<string> {
+  const [config] = await bot.drizzle
     .select({ locale: guilds.locale })
     .from(guilds)
     .where(eq(guilds.id, Number(guildId)));
 
-  return config[0].locale;
+  if (!config.locale) return "en";
+  return config.locale;
 }
