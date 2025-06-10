7ffca685ac2ca131bbbf3d37d75345e1eca88fdd
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-06-10 14:04:14 +0400

    fix: add balance command

---
+++ src/commands/balance.ts
@@ -0,0 +1,38 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { Command } from "../base/Command";
+import { bot } from "..";
+import { members } from "../schema";
+import { eq } from "drizzle-orm";
+import i18next from "i18next";
+
+export default class BalanceCommand extends Command {
+    constructor() {
+        super(new SlashCommandBuilder()
+            .setName("balance")
+            .setDescription("Check your balance")
+        )
+    }
+
+    async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+        try {
+            const [user] = await bot.drizzle
+                .select({ balance: members.balance })
+                .from(members)
+                .where(eq(members.id, BigInt(interaction.user.id)));
+
+            interaction.reply({
+                content: i18next.t(
+                    "command.balance.reply.balance",
+                    { balance: user.balance, lng: interaction.locale }
+                )
+            })
+        } catch (error) {
+            interaction.reply({
+                content: i18next.t(
+                    "command.balance.reply.balance",
+                    { balance: 0, lng: interaction.locale }
+                )
+            })
+        }
+    }
+}
