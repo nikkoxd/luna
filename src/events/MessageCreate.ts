6536f50c1619ca7696d5fdfb79e62a1047f1a596
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-06-13 15:53:57 +0400

    fix: refactor MessageCreate event

--- src/events/MessageCreate.ts
+++ src/events/MessageCreate.ts
@@ -12,19 +12,28 @@ export default class MessageCreateEvent extends Event<Events.MessageCreate> {
         super(Events.MessageCreate, false);
     }
 
-    cooldown = 5_000;
-    timestamps = new Collection<string, number>();
+    private cooldown = 5_000;
+    private timestamps = new Collection<string, number>();
 
-    async processRoles(member: GuildMember, level: number) {
-        bot.logger.info("Processing roles...")
-        bot.logger.info(`Guild id: ${member.guild.id}, level: ${level}`)
+    private isOnCooldown(userId: string) {
+        const timestamp = this.timestamps.get(userId);
+        if (timestamp) {
+            const expirationTime = timestamp + this.cooldown;
 
+            if (Date.now() < expirationTime) {
+                return true;
+            }
+        }
+        this.timestamps.set(userId, Date.now());
+        setTimeout(() => this.timestamps.delete(userId), this.cooldown)
+        return false;
+    }
+
+    private async processRewards(member: GuildMember, level: number) {
         const result = await bot.drizzle.select({ id: roles.id, level: roles.level }).from(roles).where(and(
             eq(roles.guildId, BigInt(member.guild.id)),
             gte(roles.level, level)
         ));
-
-        bot.logger.info(result);
 
         if (result.length === 0) return;
 
@@ -33,33 +42,18 @@ export default class MessageCreateEvent extends Event<Events.MessageCreate> {
             if (!guildRole) continue;
 
             member.roles.remove(guildRole);
-            bot.logger.info("Removed!");
         }
 
         const highestGuildRole = member.guild.roles.cache.get(result[result.length - 1].id.toString());
         if (!highestGuildRole) return;
 
         member.roles.add(highestGuildRole);
-        bot.logger.info("Added!");
     }
 
-    async execute(message: Message) {
-        if (
-            message.system || message.author.bot ||
-            !message.member || !message.guild || message.interactionMetadata
-        ) return;
-
-        const now = Date.now();
-        const timestamp = this.timestamps.get(message.member.id);
-        if (timestamp) {
-            const expirationTime = timestamp + this.cooldown;
-
-            if (now < expirationTime) {
-                return;
-            }
+    private async processMessage(message: Message): Promise<void> {
+        if (!message.guild) {
+            return;
         }
-        this.timestamps.set(message.member.id, now);
-        setTimeout(() => this.timestamps.delete(message.member!.id), this.cooldown)
 
         const [config] = await bot.drizzle
             .select({
@@ -72,43 +66,58 @@ export default class MessageCreateEvent extends Event<Events.MessageCreate> {
         const expToAdd = randomInt(config.minExp, config.maxExp);
         const balanceToAdd = randomInt(config.minCoins, config.maxCoins);
 
-        try {
-            await bot.drizzle.transaction(async (tx) => {
-                await tx.insert(users).values({
-                    id: BigInt(message.author.id),
-                }).onConflictDoNothing();
+        await bot.drizzle.transaction(async (tx) => {
+            await tx.insert(users).values({
+                id: BigInt(message.author.id),
+            }).onConflictDoNothing();
 
-                const beforeUpdateMember = await tx.select({ level: members.level }).from(members).where(and(
-                    eq(members.id, BigInt(message.author.id)),
-                    eq(members.guildId, BigInt(message.guild!.id))
-                ))
-                const previousLevel = beforeUpdateMember[0] ? beforeUpdateMember[0].level : 0;
+            const [beforeUpdateMember] = await tx.select({ level: members.level }).from(members).where(and(
+                eq(members.id, BigInt(message.author.id)),
+                eq(members.guildId, BigInt(message.guild!.id))
+            ))
+            const previousLevel = beforeUpdateMember ? beforeUpdateMember.level : 0;
 
-                const updatedMember = await tx.insert(members).values({
-                    id: BigInt(message.author.id),
-                    guildId: BigInt(message.guild!.id),
-                    exp: expToAdd,
-                    balance: balanceToAdd,
-                }).onConflictDoUpdate({
-                    target: [members.id, members.guildId],
-                    set: {
-                        exp: sql`${members.exp} + ${expToAdd}`,
-                        balance: sql`${members.balance} + ${balanceToAdd}`,
-                        level: sql`FLOOR((SQRT(4 * (${members.exp} + ${expToAdd}) / 50 + 1) - 1) / 2)`
-                    }
-                }).returning({ level: members.level });
-                const newLevel = updatedMember[0] ? updatedMember[0].level : 0;
-
-                if (newLevel > previousLevel) {
-                    if (!message.channel.isSendable()) return;
-
-                    this.processRoles(message.member!, newLevel);
-                    message.channel.send(i18next.t(
-                        "event.message_create.level_up",
-                        { userId: message.author.id, level: newLevel, lng: await getGuildLocale(message.guildId!) }
-                    ));
+            const [updatedMember] = await tx.insert(members).values({
+                id: BigInt(message.author.id),
+                guildId: BigInt(message.guild!.id),
+                exp: expToAdd,
+                balance: balanceToAdd,
+            }).onConflictDoUpdate({
+                target: [members.id, members.guildId],
+                set: {
+                    exp: sql`${members.exp} + ${expToAdd}`,
+                    balance: sql`${members.balance} + ${balanceToAdd}`,
+                    level: sql`FLOOR((SQRT(4 * (${members.exp} + ${expToAdd}) / 50 + 1) - 1) / 2)`
                 }
-            });
+            }).returning({ level: members.level });
+            const newLevel = updatedMember ? updatedMember.level : 0;
+
+            if (newLevel > previousLevel) {
+                if (!message.channel.isSendable()) return;
+
+                this.processRewards(message.member!, newLevel);
+                message.channel.send(i18next.t(
+                    "event.message_create.level_up",
+                    { userId: message.author.id, level: newLevel, lng: await getGuildLocale(message.guildId!) }
+                ));
+            }
+        });
+    }
+
+    public async execute(message: Message) {
+        if (
+            message.system || message.author.bot ||
+            !message.member || !message.guild || message.interactionMetadata
+        ) {
+            return;
+        }
+
+        if (this.isOnCooldown(message.author.id)) {
+            return;
+        }
+
+        try {
+            await this.processMessage(message);
         } catch (error) {
             bot.logger.error(error);
         }
