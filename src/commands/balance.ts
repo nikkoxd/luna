aa0c78403ddc63a2b6320c2a8b20523c5ff5afec
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-07-16 04:50:18 +0400

    fix: can now check balance of other users

--- src/commands/balance.ts
+++ src/commands/balance.ts
@@ -14,29 +14,66 @@ export default class BalanceCommand extends Command {
 				.setName("balance")
 				.setDescription("Check your balance")
 				.setDescriptionLocalization("ru", "Проверить баланс")
+				.addUserOption((option) =>
+					option
+						.setName("user")
+						.setDescription("The user to check balance")
+						.setDescriptionLocalization(
+							"ru",
+							"Пользователь, которому нужно проверить баланс"
+						)
+						.setRequired(false)
+				)
 		);
 	}
 
 	async execute(interaction: ChatInputCommandInteraction): Promise<void> {
-		try {
-			const [user] = await bot.db
-				.select({ balance: members.balance })
-				.from(members)
-				.where(eq(members.id, BigInt(interaction.user.id)));
+		const user = interaction.options.getUser("user");
 
-			await interaction.reply({
-				content: i18next.t("command.balance.reply.balance", {
-					balance: user.balance,
-					lng: interaction.locale,
-				}),
-			});
+		try {
+			if (user) {
+				const [userEntry] = await bot.db
+					.select({ balance: members.balance })
+					.from(members)
+					.where(eq(members.id, BigInt(user.id)));
+
+				await interaction.reply({
+					content: i18next.t("command.balance.reply.balance_2", {
+						tag: user.tag,
+						balance: userEntry.balance,
+						lng: interaction.locale,
+					}),
+				});
+			} else {
+				const [userEntry] = await bot.db
+					.select({ balance: members.balance })
+					.from(members)
+					.where(eq(members.id, BigInt(interaction.user.id)));
+
+				await interaction.reply({
+					content: i18next.t("command.balance.reply.balance", {
+						balance: userEntry.balance,
+						lng: interaction.locale,
+					}),
+				});
+			}
 		} catch {
-			await interaction.reply({
-				content: i18next.t("command.balance.reply.balance", {
-					balance: 0,
-					lng: interaction.locale,
-				}),
-			});
+			if (user) {
+				await interaction.reply({
+					content: i18next.t("command.balance.reply.balance_2", {
+						tag: user.tag,
+						balance: 0,
+						lng: interaction.locale,
+					}),
+				});
+			} else {
+				await interaction.reply({
+					content: i18next.t("command.balance.reply.balance", {
+						balance: 0,
+						lng: interaction.locale,
+					}),
+				});
+			}
 		}
 	}
 }
