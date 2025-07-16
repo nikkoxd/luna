02179b1cd238258c8797c9cbd67d28d35fa18fa9
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-07-16 04:22:59 +0400

    feat: add /block gifs and /unblock gifs

---
+++ src/commands/block.ts
@@ -0,0 +1,85 @@
+import {
+	ChatInputCommandInteraction,
+	MessageFlags,
+	SlashCommandBuilder,
+} from "discord.js";
+
+import { bot } from "..";
+import { Command } from "../base/Command";
+import i18next from "i18next";
+
+export default class BlockCommand extends Command {
+	constructor() {
+		super(
+			new SlashCommandBuilder()
+				.setName("block")
+				.setDescription("Block user's access to sth")
+				.setDescriptionLocalization(
+					"ru",
+					"Блокировка доступа к чему-то"
+				)
+				.addSubcommand((subcommand) =>
+					subcommand
+						.setName("gifs")
+						.setDescription("Block gifs")
+						.setDescriptionLocalization("ru", "Блокировка гифок")
+						.addUserOption((option) =>
+							option
+								.setName("user")
+								.setDescription("The user to block")
+								.setDescriptionLocalization(
+									"ru",
+									"Пользователь, которому нужно заблокировать гифки"
+								)
+								.setRequired(true)
+						)
+				)
+		);
+	}
+
+	public async gifs(interaction: ChatInputCommandInteraction) {
+		if (!interaction.inCachedGuild()) {
+			throw new Error("Not in guild");
+		}
+
+		const user = interaction.options.getUser("user", true);
+		const channel = interaction.channel;
+
+		if (!channel || channel.isThread()) {
+			throw new Error("Can't edit channel permissions");
+		}
+
+		try {
+			await channel.permissionOverwrites.edit(user, {
+				EmbedLinks: false,
+				AttachFiles: false,
+			});
+		} catch (error) {
+			bot.logger.error(
+				`Error while editing channel permissions: ${error}`
+			);
+
+			await interaction.reply({
+				content: i18next.t("command.block.reply.error"),
+				flags: [MessageFlags.Ephemeral],
+			});
+		} finally {
+            await interaction.reply({
+                content: i18next.t("command.block.gifs.reply.success", {
+                    userId: user.id,
+                }),
+                flags: [MessageFlags.Ephemeral],
+            });
+        }
+	}
+
+	async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+		const subcommand = interaction.options.getSubcommand();
+		switch (subcommand) {
+			case "gifs": {
+				await this.gifs(interaction);
+				break;
+			}
+		}
+	}
+}
---
+++ src/commands/unblock.ts
@@ -0,0 +1,92 @@
+import {
+	ChatInputCommandInteraction,
+	MessageFlags,
+	SlashCommandBuilder,
+} from "discord.js";
+
+import i18next from "i18next";
+
+import { bot } from "..";
+import { Command } from "../base/Command";
+
+export default class UnblockCommand extends Command {
+	constructor() {
+		super(
+			new SlashCommandBuilder()
+				.setName("unblock")
+				.setDescription("Unblock user's access to sth")
+				.setDescriptionLocalization(
+					"ru",
+					"Разблокировка доступа к чему-то"
+				)
+				.addSubcommand((subcommand) =>
+					subcommand
+						.setName("gifs")
+						.setDescription("Unblock gifs")
+						.setDescriptionLocalization("ru", "Разблокировка гифок")
+						.addUserOption((option) =>
+							option
+								.setName("user")
+								.setDescription("The user to unblock")
+								.setDescriptionLocalization(
+									"ru",
+									"Пользователь, которому нужно разблокировать гифки"
+								)
+								.setRequired(true)
+						)
+				)
+		);
+	}
+
+	public async gifs(interaction: ChatInputCommandInteraction) {
+		if (!interaction.inCachedGuild()) {
+			throw new Error("Not in guild");
+		}
+
+		const user = interaction.options.getUser("user", true);
+		const channel = interaction.channel;
+
+		if (!channel || channel.isThread()) {
+			throw new Error("Can't edit channel permissions");
+		}
+
+		try {
+			await channel.permissionOverwrites.edit(user, {
+				EmbedLinks: null,
+				AttachFiles: null,
+			});
+
+			// if no permissions are set, delete the overwrite
+			const overwrite = channel.permissionOverwrites.cache.get(user.id);
+			if (overwrite?.deny.toArray().length === 0) {
+				await channel.permissionOverwrites.delete(user);
+			}
+		} catch (error) {
+			bot.logger.error(
+				`Error while editing channel permissions: ${error}`
+			);
+
+			await interaction.reply({
+				content: i18next.t("command.unblock.reply.error"),
+				flags: [MessageFlags.Ephemeral],
+			});
+		} finally {
+			await interaction.reply({
+				content: i18next.t("command.unblock.gifs.reply.success", {
+					userId: user.id,
+				}),
+				flags: [MessageFlags.Ephemeral],
+			});
+		}
+	}
+
+	async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+		const subcommand = interaction.options.getSubcommand();
+		switch (subcommand) {
+			case "gifs": {
+				await this.gifs(interaction);
+				break;
+			}
+		}
+	}
+}
